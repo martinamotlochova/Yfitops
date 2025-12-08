de29b11700c36cc6962416b4554bef3d07e8a63a
Martina Motlochová <matamotlochova@gmail.com>
2025-12-08 09:44:59 +0100

    ADD: MudBlazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Yfitops.Client;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +15,6 @@ builder.Services.AddHttpClient("Yfitops.ServerAPI", client => client.BaseAddress
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Yfitops.ServerAPI"));
 
 builder.Services.AddApiAuthorization();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
