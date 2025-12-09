a339dc5a92dafda464b718f165cb88577362d134
Martina Motlochová <matamotlochova@gmail.com>
2025-12-09 20:53:31 +0100

    ADD: UserContract

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Yfitops.Shared;
 
 namespace Yfitops.Server.Models;
 
@@ -7,4 +8,25 @@ public class ApplicationUser : IdentityUser
     public List<Artist> FavoriteArtists {get; set; } = new List<Artist>();
     public List<Album> FavoriteAlbums {get; set; } = new List<Album>();
     public List<Track> FavoriteTracks {get; set; } = new List<Track>();
+
+    public static ApplicationUser ToEntity(UserContract contract)
+    {
+        return new ApplicationUser()
+        {
+            Id = contract.Id,
+            UserName = contract.UserName,
+            Email = contract.Email,
+
+        };
+    }
+
+    public static UserContract ToContract(ApplicationUser user)
+    {
+        return new UserContract()
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+        };
+    }
 }
---
+++ Shared/UserContract.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Yfitops.Shared;
+
+public class UserContract
+{
+    public string Id { get; set; } 
+    
+    public string UserName { get; set; } 
+    
+    public string Email { get; set; } 
+    
+}
