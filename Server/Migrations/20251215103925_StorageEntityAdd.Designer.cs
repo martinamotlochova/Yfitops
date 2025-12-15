df54a811d86a8575e518bccaa28092fb09e857ac
Martina Motlochová <matamotlochova@gmail.com>
2025-12-15 11:40:38 +0100

    Migration

--- Server/Migrations/20251215103925_StorageEntityAdd.Designer.cs
+++ Server/Migrations/20251215103925_StorageEntityAdd.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yfitops.Server.Data;
 
@@ -10,9 +11,11 @@ using Yfitops.Server.Data;
 namespace Yfitops.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215103925_StorageEntityAdd")]
+    partial class StorageEntityAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
---
+++ Server/Migrations/20251215103925_StorageEntityAdd.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yfitops.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class StorageEntityAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "StogareId",
+                table: "Tracks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StorageId",
+                table: "Tracks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Albums",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FileName = table.Column<string>(type: "TEXT", nullable: true),
+                    Data = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Size = table.Column<long>(type: "INTEGER", nullable: false),
+                    UploadedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_StorageId",
+                table: "Tracks",
+                column: "StorageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tracks_Storages_StorageId",
+                table: "Tracks",
+                column: "StorageId",
+                principalTable: "Storages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tracks_Storages_StorageId",
+                table: "Tracks");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tracks_StorageId",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "StogareId",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "StorageId",
+                table: "Tracks");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
