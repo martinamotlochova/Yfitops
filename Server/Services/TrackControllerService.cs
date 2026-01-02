f9c845feb811636f6233b7507f2baba3e0d1e404
martinamotlochova <137939946+martinamotlochova@users.noreply.github.com>
2026-01-02 15:16:36 +0100

    ADD: Service layer and implemented one method from TrackController

---
+++ Server/Services/TrackControllerService.cs
@@ -0,0 +1,26 @@
+﻿using Yfitops.Server.Data;
+using Yfitops.Server.Models;
+using Yfitops.Shared;
+
+namespace Yfitops.Server.Services
+{
+    public class TrackControllerService
+    {
+        private ApplicationDbContext context;
+
+        public TrackControllerService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<TrackContract> GetTrackByIdAsync(Guid id, string currentUserId)
+        {
+            var track = await context.Tracks.FindAsync(id);
+            if (track == null)
+            {
+                return null;
+            }
+            return Track.ToContract(track, currentUserId);
+        }
+    }
+}
